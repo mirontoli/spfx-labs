3d2cd95200661119e1ba939f4d800ab4b65f73fe
Anatoly Mironov <mirontoli@gmail.com>
2020-10-02 08:28:03 +0200

    initial commit

---
+++ spfx009-ext/src/extensions/spfx009Ext/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISpfx009ExtApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'Spfx009ExtApplicationCustomizerStrings' {
+  const strings: ISpfx009ExtApplicationCustomizerStrings;
+  export = strings;
+}
